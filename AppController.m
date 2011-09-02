56cef00087f667438bbd7b62306447e5b4bae4c7
Florian Ebeling <florian.ebeling@gmail.com>
2011-09-02 21:22:53 +0200

    Manage memory

--- AppController.m
+++ AppController.m
@@ -11,8 +11,8 @@
 
 @implementation AppController
 
-- (void) awakeFromNib{
-	
+- (void)awakeFromNib
+{
 	//Create the NSStatusBar and set its length
 	statusItem = [[[NSStatusBar systemStatusBar] statusItemWithLength:NSSquareStatusItemLength] retain];
 	
@@ -24,11 +24,14 @@
 	[statusItem setHighlightMode:YES];
 }
 
-//- (void) dealloc {
-//	[super dealloc];
-//}
+- (void)dealloc
+{
+  [statusItem release];
+	[super dealloc];
+}
 
--(IBAction)openDoor:(id)sender{
+- (IBAction)openDoor:(id)sender
+{
 	NSLog(@"Trying to open the Door");
 	
 	// Setup a request
@@ -38,22 +41,23 @@
                                                      timeoutInterval:5.0];
 	[request setHTTPMethod:@"GET"];
 	
-	// Open the connection¬¬
-  [[NSURLConnection alloc] initWithRequest:request delegate:self]; 
+	// Open the connection
+  connection = [[NSURLConnection alloc] initWithRequest:request delegate:self]; 
 }
 
 // Everything is fine
-- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+- (void)connection:(NSURLConnection *)theConnection didReceiveResponse:(NSURLResponse *)response
 {
 	[connection release];
+  connection = nil;
 	NSLog(@"Door opened.");
 }
 
 // Something went wrong
-- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+- (void)connection:(NSURLConnection *)theConnection didFailWithError:(NSError *)error
 {
-    [connection release];
-	
+  [connection release];
+  connection = nil;
 	NSLog(@"Doorduino failed!");
     //NSLog(@"Connection Error - %@ %@",
     //      [error localizedDescription]);
